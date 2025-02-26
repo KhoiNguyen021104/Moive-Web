b04140d1f3d358fe9fb64b429b1db2aa77a9ef21
KhoiNguyenDev <khoindt10a4@gmail.com>
2025-02-26 12:32:58 +0700

    fix banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -32,39 +32,44 @@ function Banner() {
     fetch();
   }, []);
   return (
-    <div className={cx("banner_wrapper")}>
-      <Carousel {...settings} autoplay>
-        {movies?.map((item, index) => (
-          <div className={cx("slide")} key={index}>
-            <img src={item?.thumb_url} alt='' />
-            <div className={cx("banner__info")}>
-              <h1>{item?.name}</h1>
-              <div className={cx("btn__group")}>
-                <button
-                  onClick={() => navigate(`/mo-ta-phim/${slugify(item?.name)}`)}
-                  className={cx("btn", "btn-play")}
-                >
-                  <Play />
-                  Xem chi tiết
-                </button>
-                <button
-                  onClick={() => setIsMark((prev) => !prev)}
-                  className={cx("btn", "btn-save")}
-                >
-                  <Bookmark className={cx(isMark && "active")} />
-                </button>
-                <button
-                  onClick={() => setIsLike((prev) => !prev)}
-                  className={cx("btn", "btn-like")}
-                >
-                  <Heart className={cx(isLike && "active")} />
-                </button>
+    movies?.length >
+    0(
+      <div className={cx("banner_wrapper")}>
+        <Carousel {...settings} autoplay>
+          {movies?.map((item, index) => (
+            <div className={cx("slide")} key={index}>
+              <img src={item?.thumb_url} alt='' />
+              <div className={cx("banner__info")}>
+                <h1>{item?.name}</h1>
+                <div className={cx("btn__group")}>
+                  <button
+                    onClick={() =>
+                      navigate(`/mo-ta-phim/${slugify(item?.name)}`)
+                    }
+                    className={cx("btn", "btn-play")}
+                  >
+                    <Play />
+                    Xem chi tiết
+                  </button>
+                  <button
+                    onClick={() => setIsMark((prev) => !prev)}
+                    className={cx("btn", "btn-save")}
+                  >
+                    <Bookmark className={cx(isMark && "active")} />
+                  </button>
+                  <button
+                    onClick={() => setIsLike((prev) => !prev)}
+                    className={cx("btn", "btn-like")}
+                  >
+                    <Heart className={cx(isLike && "active")} />
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </Carousel>
-    </div>
+          ))}
+        </Carousel>
+      </div>
+    )
   );
 }
 
